65d1c3d9bc2f04a0e2dbeb066a4994bd7e7b59d5
Nigel-23047846 <23047846@myrp.edu.sg>
2024-12-02 15:00:32 +0800

    Finally done

--- src/components/AddHabitForm.js
+++ src/components/AddHabitForm.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 
-export default function AddHabitForm({ onAddHabit }) {
+export default function AddHabitForm({ handleAddHabit }) {
   const [habitName, setHabitName] = useState("");
 
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
-  };
-
-  return {
+    e.preventDefault();
+    handleAddHabit(habitName);
+    setHabitName("");
+  }
+  return (
     //TODO: add a form to add a new habit
-  };
+    <form className="add-form" onSubmit={handleSubmit} >
+      <input type="text"
+        value={habitName}
+        onChange={(e) => setHabitName(e.target.value)}
+        placeholder="New Habit" />
+      <button type="submit">Add Habit</button>
+    </form>
+  );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,37 @@ function App() {
 
   const handleAddHabit = (habitName) => {
     // TODO: write code to add a new habit here
+    const newHabit = {
+      id: Date.now(),
+      name: habitName,
+      completed: false,
+    };
+    setHabits([...habits, newHabit]);
   };
 
   const handleToggleHabit = (id) => {
     // TODO: write code to toggle a habit's status
+    setHabits((prevHabits) => prevHabits.map((habit) =>
+      habit.id === id ? { ...habit, completed: !habit.completed } : habit
+    ));
   };
 
   const handleDeleteHabit = (id) => {
     // TODO: write code to delete a habit
+    setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
   };
 
   return (
-    <div>
+    <div className="App">
       <h1>Habit Tracker</h1>
       {/*TODO: add a form to add a new habit*/}
       {/*TODO: add a list of habits*/}
+      <AddHabitForm handleAddHabit={handleAddHabit} />
+      <HabitList
+        habits={habits}
+        onToggleHabit={handleToggleHabit}
+        onDeleteHabit={handleDeleteHabit}
+      />
     </div>
   );
 }
